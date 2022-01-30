aad7c9846c0f1039715d8e1977500c54c79c3ac7
npanium <npanium@gmail.com>
2022-01-30 21:18:44 +1100

    added movies and movie details on work page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import AboutSection from "./components/AboutSection";
 import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" element={<AboutUs />} />
+        <Route path="work" element={<OurWork />} />
+        <Route path="work/:id" element={<MovieDetail />} />
+        <Route path="contact/*" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
