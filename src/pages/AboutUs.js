6daa96ac087a0618a2ace5833a899d54a44c52c3
npanium <npanium@gmail.com>
2022-02-08 14:40:54 +1100

    added page animations using framer motion. Made changes to App.js for it to work on exit

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,13 +4,22 @@ import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
 import FaqSection from "../components/FaqSection";
 
+//animation
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
+
 const AboutUs = () => {
   return (
-    <>
+    <motion.div
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+    >
       <AboutSection />
       <ServicesSection />
       <FaqSection />
-    </>
+    </motion.div>
   );
 };
 
